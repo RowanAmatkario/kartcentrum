c837ec05d4738bf47dc66885efee291f010bb7a6
roama <rowan.amatkario@eonics.nl>
2021-05-15 21:08:44 +0200

    proberen voor het fixen van de datum

--- src/Repository/ActiviteitRepository.php
+++ src/Repository/ActiviteitRepository.php
@@ -52,32 +52,4 @@ class ActiviteitRepository extends ServiceEntityRepository
 
     }
 
-    // /**
-    //  * @return Activiteit[] Returns an array of Activiteit objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Activiteit
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
